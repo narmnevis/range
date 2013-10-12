f8e086f871d7eb2292d05272357c3955fae9f1c7
Behrooz Nobakht <behrooz.nobakht@gmail.com>
2013-10-12 18:02:16 +0200

    Issue #4 DateFormat is not thread safe

--- src/main/java/com/narmnevis/range/generator/DateGenerator.java
+++ src/main/java/com/narmnevis/range/generator/DateGenerator.java
@@ -3,7 +3,6 @@ package com.narmnevis.range.generator;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.narmnevis.range.Generator;
@@ -12,10 +11,8 @@ import com.narmnevis.range.RangeContext;
 public class DateGenerator extends AbstractGenerator implements Generator {
 
 	private static final long ONE_YEAR = TimeUnit.DAYS.toMillis(365);
-	private static final DateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	private final Date start;
-	private List<Double> samples;
 
 	public DateGenerator() {
 		this.start = new Date(System.currentTimeMillis() - ONE_YEAR);
@@ -23,13 +20,12 @@ public class DateGenerator extends AbstractGenerator implements Generator {
 
 	@Override
 	public Object generate(RangeContext context) {
-		if (samples == null) {
-			samples = generateGaussianRandoms(context);
-		}
 		try {
-			Double d = samples.remove(0);
+			// Costly but thread-safe
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Double d = getNextRandom(context);
 			long time = start.getTime() + new Double(ONE_YEAR * d).longValue();
-			return FORMAT.format(new Date(time));
+			return format.format(new Date(time));
 		} catch (Exception e) {
 			logger.error("Failed to generate a date: {}", e);
 		}
